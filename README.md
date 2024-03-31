59f96ed0d4c6969c6e4ffee100e235a73b8ec2c3
GregTheMadMonk <yagreg7@gmail.com>
2024-03-31 05:07:36 +0300

    Start implementing math for MHFE. There's an issue

--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 
 This repo is an eazy-to-setup place to experiment with using Enzyme from C++.
 
-Tests, their status and requirements:
-1. [eigen-simple](https://github.com/GregTheMadMonk/enzyme-tests/tree/master/tests/eigen-simple)
-   a simple example that uses Eigen 3.4.0 with Enzyme.
-2. [eigensumsqdyn-notmp](https://github.com/GregTheMadMonk/enzyme-tests/tree/master/tests/eigensumsqdyn-notmp)
-   another Eigen+Enzyme example, taken from directly from Enzyme tests. Doesn't
-   compile for an unknown reason. See [Enzyme#1800](https://github.com/EnzymeAD/Enzyme/issues/1800)
-   for status updates.
-3. [multi-tu](https://github.com/GregTheMadMonk/enzyme-tests/tree/master/tests/multi-tu)
-   two translation units + Enzyme during linking (LLDEnzyme).
+## [eigen-simple](https://github.com/GregTheMadMonk/enzyme-tests/tree/master/tests/eigen-simple)
+A simple example that uses Eigen 3.4.0 with Enzyme.
+
+## [eigensumsqdyn-notmp](https://github.com/GregTheMadMonk/enzyme-tests/tree/master/tests/eigensumsqdyn-notmp)
+Another Eigen+Enzyme example, taken from directly from Enzyme tests. Doesn't
+compile for an unknown reason. See [Enzyme#1800](https://github.com/EnzymeAD/Enzyme/issues/1800)
+for status updates.
+
+## [multi-tu](https://github.com/GregTheMadMonk/enzyme-tests/tree/master/tests/multi-tu)
+Two translation units + Enzyme during linking (LLDEnzyme).
+
+## [mhfem](https://github.com/GregTheMadMonk/enzyme-tests/tree/master/tests/mhfem)
+MHFEM differentiation with Enzyme.
+
+Uses C++20 modules and some C++23 features. Needs **Ninja**, at least **CMake
+3.28**, and an LLVM build that supports the following features:
+1. Deducing this
+2. Multi-index `operator[]`
+3. C++20 modules support
+4. A standard library implementation that supports `import std` (LLVM 19)
---
+++ tests/mhfem/main.cc
@@ -0,0 +1,112 @@
+import std;
+import stdm;
+
+#include <c-enzyme.hh>
+
+namespace stdr = std::ranges;
+
+int main() {
+    std::println("Test stdm::dot()...");
+
+    std::vector<float>  v{ 1, 2, 3 };
+    std::vector<double> u{ 3, 2, 1 };
+    std::println("v @ u = {} @ {} = {}", v, u, stdm::dot(v, u));
+    stdm::assert(stdm::dot(v, u) == 10);
+
+    decltype(v) dv(v.size());
+    decltype(u) du(u.size());
+
+    __enzyme_autodiff(
+        stdm::dot<std::vector<float>, std::vector<double>>,
+        enzyme_dup, &v, &dv,
+        enzyme_dup, &u, &du
+    );
+
+    std::println("d/dv = {}", dv);
+    stdm::assert(stdr::equal(dv, u));
+    std::println("d/du = {}", du);
+    stdm::assert(stdr::equal(du, v));
+
+
+    std::println("Test stdm::matvec()...");
+
+    std::vector<float> M{
+        0, 1, 0,
+        0, 0, 1,
+        1, 0, 0
+    };
+    std::println("M @ v = {} @ {} = {}", M, v, stdm::matvec(M, v));
+    stdm::assert(stdr::equal(stdm::matvec(M, v), std::vector{ 2, 3, 1 }));
+
+    decltype(M) dM(M.size());
+    for (auto i = 0uz; i < stdr::size(v); ++i) {
+        std::println("Component #{}", i);
+        stdr::fill(dM, 0);
+        stdr::fill(dv, 0);
+
+        __enzyme_autodiff(
+            stdm::matvec_element<decltype(M), decltype(v)>,
+            enzyme_dup, &M, &dM,
+            enzyme_dup, &v, &dv,
+            enzyme_const, i
+        );
+        std::println("d/dM = {}", dM);
+        std::println("d/dv = {}", dv);
+    }
+
+
+    std::println("Test stdm::vecmat()...");
+    std::println("v.T @ M = {}.T @ {} = {}", v, M, stdm::vecmat(v, M));
+    stdm::assert(stdr::equal(stdm::vecmat(v, M), std::vector{ 3, 1, 2 }));
+
+    for (auto i = 0uz; i < stdr::size(v); ++i) {
+        std::println("Component ${}", i);
+        stdr::fill(dM, 0);
+        stdr::fill(dv, 0);
+
+        __enzyme_autodiff(
+            stdm::vecmat_element<decltype(M), decltype(v)>,
+            enzyme_dup, &v, &dv,
+            enzyme_dup, &M, &dM,
+            enzyme_const, i
+        );
+        std::println("d/dM = {}", dM);
+        std::println("d/dv = {}", dv);
+    }
+
+
+    std::println("Test sqlen() lambda...");
+    static constexpr auto sqlen = [] (const std::vector<float>& x) {
+        return stdm::dot(x, x);
+    };
+
+
+    std::println("{} length squared is {}", v, sqlen(v));
+
+    stdr::fill(dv, 0);
+    __enzyme_autodiff(+sqlen, enzyme_dup, &v, &dv);
+    std::println("d(sqlen)/dv = {}", dv);
+    stdm::assert(stdr::equal(dv, std::vector{ 2, 4, 6 }));
+    std::println("Test vector length converter...");
+    static constexpr auto len_after =
+        [] (const std::vector<float>& m, const std::vector<float>& x) {
+            const auto mx = stdm::matvec(m, x);
+            return stdm::dot(mx, mx);
+        };
+
+
+    std::println(
+        "Length squared of {} after applying {} is {} (old length squared {})",
+        v, M, len_after(M, v), stdm::dot(v, v)
+    );
+    stdr::fill(dM, 0);
+
+    /* This differentiation fails!
+    __enzyme_autodiff(
+        +len_after,
+        enzyme_dup, &M, &dM,
+        enzyme_const, &v
+    );
+    */
+    return 0;
+}
